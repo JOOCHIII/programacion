cc9f38701dd6f14baaeea67bec30bbd2907927ac
jorge <jorge@DESKTOP-KKB65N3>
2024-01-30 17:38:24 +0100

    POO TODOS

---
+++ POOTOTAL/POO/CONTACTO/Contacto.java
@@ -0,0 +1,51 @@
+package CONTACTO;
+
+public class Contacto {
+
+    private String nombreContacto;
+    private String numeroTelefono;
+    private String emailContacto;
+
+    // Constructor
+    public Contacto(String nombre, String num, String email) {
+        this.nombreContacto = nombre;
+        this.numeroTelefono = num;
+        this.emailContacto = email;
+    }
+
+    // Getters y setters
+    public String getNombreContacto() {
+        return nombreContacto;
+    }
+
+    public void setNombreContacto(String nombreContacto) {
+        this.nombreContacto = nombreContacto;
+    }
+
+    public String getNumeroTelefono() {
+        return numeroTelefono;
+    }
+
+    public void setNumeroTelefono(String numeroTelefono) {
+        this.numeroTelefono = numeroTelefono;
+    }
+
+    public String getEmailContacto() {
+        return emailContacto;
+    }
+
+    public void setEmailContacto(String emailContacto) {
+        this.emailContacto = emailContacto;
+    }
+
+    // Método para mostrar información del contacto
+    public String mostrarContacto() {
+        String menu = "Nombre del contacto: " + this.nombreContacto;
+        menu += "\n---------------------------------------\n";
+        menu += "\nNúmero del contacto: " + this.numeroTelefono;
+        menu += "\n---------------------------------------\n";
+        menu += "\nEmail del contacto: " + this.emailContacto + "\n";
+        menu += "\n---------------------------------------\n";
+        return menu;
+    }
+}
