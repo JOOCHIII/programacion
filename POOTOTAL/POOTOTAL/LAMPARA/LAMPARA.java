55e21b4e1b000c6dd45a3575d325c52aa09b20ca
jorgediazmarrero <jorgediazmarr@gmail.com>
2024-01-30 12:59:23 +0100

    SIIII ME VA YA SE

---
+++ POOTOTAL/POOTOTAL/LAMPARA/LAMPARA.java
@@ -0,0 +1,24 @@
+package LAMPARA;
+
+<<<<<<< HEAD
+public class LAMPARA {
+String tipo;
+int potencia;
+String color;
+
+public  LAMPARA() {
+=======
+public class LAMPARA {String tipo;
+int potencia;
+String color;
+
+public Lampara() {
+>>>>>>> branch 'master' of https://github.com/JOOCHIII/programacion
+
+tipo="LED";
+potencia=60;
+color="Blanco";
+
+}
+
+}
---
+++ POOTOTAL/POOTOTAL/LIBRO/Libro.java
@@ -0,0 +1,19 @@
+<<<<<<< HEAD
+package LIBRO;
+=======
+package ejercicios1;
+>>>>>>> branch 'master' of https://github.com/JOOCHIII/programacion
+
+public class Libro {
+	String titulo;
+	int numPaginas;
+	String autor;
+	
+public Libro() {
+	
+	titulo= "Antes de diciembre";
+	autor="Desconocido";
+	numPaginas=500;
+}
+
+}
