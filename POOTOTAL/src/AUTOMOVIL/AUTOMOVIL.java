5a263660563f240e8c7adb3ff4947872314228e6
jorgediazmarrero <jorgediazmarr@gmail.com>
2024-01-31 10:22:35 +0100

    poo total

---
+++ POOTOTAL/src/AUTOMOVIL/AUTOMOVIL.java
@@ -0,0 +1,16 @@
+package AUTOMOVIL;
+
+public class AUTOMOVIL {
+    private String marca;
+    private String modelo;
+
+    // Setter
+    public void establecerMarca() {
+        marca = "Mercedes";
+    }
+
+    // Getter
+    public String obtenerMarca() {
+        return "Mi coche es un " + marca;
+    }
+}
---
+++ POOTOTAL/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module POOTOTAL {
+	requires java.desktop;
+}
